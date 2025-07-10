1a475bca0b7bc7645c5ed28b1dfb44ba32ca9338
depoco <depoco.online@gmail.com>
2025-07-10 23:05:22 +0200

    Changes to PATH

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/snub/',
+  base: '/snub-extension/',
   plugins: [tailwindcss()],
 })
